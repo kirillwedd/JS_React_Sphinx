004253e97b4821e5150675f670477d430f5dcd91
kirillwedd <kirillwed23@yandex.ru>
2024-05-20 04:03:05 +0500

    BodyCatalog Filter  Range Complited

---
+++ src/Components/MiddleBody/BodyCatalog/CategoryProduct/BrandsSelect/BrandsSelect.jsx
@@ -0,0 +1,38 @@
+import '../BrandsSelect/BrandsSelect.css'
+
+function BrandsSelect() {
+    var expanded = false;
+
+    function showCheckboxes() {
+      var checkboxes = document.getElementById("checkboxes");
+      if (!expanded) {
+        checkboxes.style.display = "block";
+        expanded = true;
+      } else {
+        checkboxes.style.display = "none";
+        expanded = false;
+      }
+    }
+
+  return (
+    <div class="multiselect" style={{marginTop:"15px"}}>
+    <div class="selectBox" onClick={showCheckboxes}>
+      <select>
+        <option>Select an option</option>
+      </select>
+      <div class="overSelect"></div>
+    </div>
+    <div id="checkboxes">
+      <label for="one">
+        <input type="checkbox" id="one" />First checkbox</label>
+      <label for="two">
+        <input type="checkbox" id="two" />Second checkbox</label>
+      <label for="three">
+        <input type="checkbox" id="three" />Third checkbox</label>
+    </div>
+    
+  </div>
+  );
+}
+
+export default BrandsSelect;
--- src/Components/MiddleBody/BodyCatalog/CategoryProduct/CategoryProduct.jsx
+++ src/Components/MiddleBody/BodyCatalog/CategoryProduct/CategoryProduct.jsx
@@ -1,12 +1,78 @@
+import { useState } from "react";
+import "../CategoryProduct/CategoryProduct.css";
+import BrandsSelect from "./BrandsSelect/BrandsSelect";
+
+
+
 function CategoryProduct() {
+  const [minValue, setValue] = useState(2500);
+  const[minLeft, setLeft]=useState(25);
+  const[minRight, setRight]=useState(25);
+  const [minValue2, setValue2] = useState(7500);
+  // Сделать состояние специальное для left right
+  
+  const onChangeHandlerRange1 = (event) => {
+    if(minValue.valueOf()<minValue2.valueOf())
+      {
+        setValue(event.target.value);
+      }
+      else
+      {
+        setValue(2500);
+      }  
+
+        setLeft(()=> {
+          const progressLeft=(minValue) / 100;
+          return progressLeft
+        })
+       
+      
+  };
+
+  const onChangeHandlerRange2 = (event) => {
+    if(minValue2.valueOf()> minValue.valueOf())
+      {
+        setValue2(event.target.value);
+      }
+      else
+      {
+       
+        setValue2(7500);
+      }
+
+    setRight(()=> {
+      const progressRight=100-(minValue2)/100;
+      return progressRight
+    })
+  };
+
+
+
   return (
-    <div>
-      <form className="filter-form">
-        <div className="block-range">
-          <span>uiuhuihi</span>
-          <input type="range" min="0" max="100" value="0"></input>
-        </div>
-      </form>
+    <div className="wrapper">
+      <header>
+        <h2>Цена</h2>
+        <p>Укажите минимальную и максимальную цену</p>
+      </header>
+      <div className="price-input">
+       <div className="field">
+        <span>Min</span>
+        <input type="number" className="input-min" value={minValue}/>
+       </div>
+       <div className="separator">-</div>
+       <div className="field">
+        <span>Max</span>
+        <input type="text" className="input-max"   step={1} value={minValue2} />
+       </div>
+      </div>
+      <div className="sliders">
+        <div className="progress" style={{left: `${minLeft +"%"}`, right:`${minRight + "%"}`}}></div>
+      </div>
+      <div className="range-input">
+        <input type="range" className="range-min" min={0} max={10000} step={1} value={minValue} onChange={onChangeHandlerRange1}   />
+        <input type="range" className="range-max" min={0} max={10000} step={1} value={minValue2} onChange={onChangeHandlerRange2} />
+      </div>
+      <BrandsSelect></BrandsSelect>
     </div>
   );
 }
