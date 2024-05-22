18fd8e59abcb0bbe164a27b0191ed15b6241238d
kirillwedd <kirillwed23@yandex.ru>
2024-05-23 03:12:04 +0500

    BodyCatalog Filter  checkbox clear

--- src/Components/MiddleBody/BodyCatalog/CategoryProduct/BrandsSelect/BrandsSelect.jsx
+++ src/Components/MiddleBody/BodyCatalog/CategoryProduct/BrandsSelect/BrandsSelect.jsx
@@ -6,24 +6,31 @@ import { country } from "../../../../data";
 import { useRef, useEffect, useState } from "react";
 
 function BrandsSelect() {
-const [checked, setChecked]=useState(false)
-
-function handle(type)
-{
-setChecked(type)
-
-}
-
-const handleChange=(event)=>{
-setChecked(event.currentTarget.checked)
-}
-
-  const listItemsCountry = country.map((person, index) => 
-  
-  (<Checkbox checked={checked} onChange={handleChange} key={index}>{person}</Checkbox>)
+  const [checkboxes, setCheckboxes] = useState(
+    Array(country.length).fill(false)
   );
 
- 
+  const handleCheckboxChange = (index) => {
+    const newCheckboxes = [...checkboxes];
+    newCheckboxes[index] = !newCheckboxes[index];
+
+    setCheckboxes(newCheckboxes);
+  };
+
+  const listItemsCountry = country.map((person, index) => (
+    <Checkbox
+      checked={checkboxes[index]}
+      key={index}
+      onChange={() => handleCheckboxChange(index)}
+    >
+      {person}
+    </Checkbox>
+  ));
+
+  const handleRemoveChecked = () => {
+    const newCheckboxes = checkboxes.map(() => false);
+    setCheckboxes(newCheckboxes);
+  };
 
   const [bool, setBool] = useState(false);
   const [inputСondition, setInputСondition] = useState({
@@ -59,59 +66,56 @@ setChecked(event.currentTarget.checked)
     }
   }
 
- 
-
-    return (
-      <div class="multiselect" style={{ marginTop: "15px" }}>
-        <div class="selectBox" onClick={() => showCheckboxBrands(!bool)}>
-          <select>
-            <option>Выберите бренд</option>
-          </select>
-          <div class="overSelect"></div>
-        </div>
-        <div
-          className="checkboxes-brands"
-          style={{ display: `${inputСondition.brands}` }}
-        >
-          <Checkbox fors={"jhkjk"}>Royal Canin</Checkbox>
-          <Checkbox fors={"jhkjk"}>Hill's</Checkbox>
-          <Checkbox fors={"jhkjk"}>Tommy</Checkbox>
-          <Checkbox fors={"jhkjk"}>Дарэлл</Checkbox>
-        </div>
-
-        <div class="selectBox" onClick={() => showCheckboxTypeFeed(!bool)}>
-          <select>
-            <option>Тип корма</option>
-          </select>
-          <div class="overSelect"></div>
-        </div>
-        <div
-          className="checkboxes-type-feed"
-          style={{ display: `${inputСondition.typeFeed}` }}
-        >
-          <Checkbox  fors={"jhkjk"}>
-            Сухой
-          </Checkbox>
-          <Checkbox fors={"jhkjk"}>Влажный</Checkbox>
-        </div>
-
-        <div class="selectBox" onClick={() => showCheckboxCountry(!bool)}>
-          <select>
-            <option>Страна производителя</option>
-          </select>
-          <div class="overSelect"></div>
-        </div>
-        <div
-          className="checkboxes-country"
-          style={{ display: `${inputСondition.countrys}` }}
-        >
-          {listItemsCountry}
-        </div>
-        <div>
-          <input type="button" onClick={()=>handle(!checked)} value="Сбросить" />
-          <input type="button" value="Сохранить" />
-        </div>
+  return (
+    <div class="multiselect" style={{ marginTop: "15px" }}>
+      <div class="selectBox" onClick={() => showCheckboxBrands(!bool)}>
+        <select>
+          <option>Выберите бренд</option>
+        </select>
+        <div class="overSelect"></div>
       </div>
-    );
-  }
+      <div
+        className="checkboxes-brands"
+        style={{ display: `${inputСondition.brands}` }}
+      >
+        <Checkbox fors={"jhkjk"}>Royal Canin</Checkbox>
+        <Checkbox fors={"jhkjk"}>Hill's</Checkbox>
+        <Checkbox fors={"jhkjk"}>Tommy</Checkbox>
+        <Checkbox fors={"jhkjk"}>Дарэлл</Checkbox>
+      </div>
+
+      <div class="selectBox" onClick={() => showCheckboxTypeFeed(!bool)}>
+        <select>
+          <option>Тип корма</option>
+        </select>
+        <div class="overSelect"></div>
+      </div>
+      <div
+        className="checkboxes-type-feed"
+        style={{ display: `${inputСondition.typeFeed}` }}
+      >
+        <Checkbox fors={"jhkjk"}>Сухой</Checkbox>
+        <Checkbox fors={"jhkjk"}>Влажный</Checkbox>
+      </div>
+
+      <div class="selectBox" onClick={() => showCheckboxCountry(!bool)}>
+        <select>
+          <option>Страна производителя</option>
+        </select>
+        <div class="overSelect"></div>
+      </div>
+      <div
+        className="checkboxes-country"
+        style={{ display: `${inputСondition.countrys}` }}
+      >
+        {listItemsCountry}
+      </div>
+      <div className="button-filters">
+        <input type="button" onClick={handleRemoveChecked} value="Сбросить" />
+        <input type="button" value="Применить" />
+      </div>
+    </div>
+  );
+}
+
 export default BrandsSelect;
